826db5e58e8e8cb509f3d6d8ab0d51d0419bd54c
Isak Kristola <iskr1300@student.miun.se>
2021-02-27 11:08:30 +0100

    Maven support

--- src/test/java/org/matrixertest/filesaver/TestFileSaver.java
+++ src/test/java/org/matrixertest/filesaver/TestFileSaver.java
@@ -6,7 +6,7 @@ import java.nio.file.Path;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class FileSaverTest {
+class TestFileSaver {
 
     @Test
     void testSaveText() {
--- src/test/java/org/matrixertest/printer/TestPrinter.java
+++ src/test/java/org/matrixertest/printer/TestPrinter.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class PrinterTest {
+class TestPrinter {
 
     @Test
     void testPrintInput() {
